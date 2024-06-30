74d262eb56ce8d651e45ec25e4881bb684855f98
18612849621 <1354368242@qq.com>
2024-06-30 21:50:10 +0800

    add treesitter

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -50,6 +50,14 @@ return require('packer').startup(function(use)
             'nvim-telescope/telescope.nvim', tag = '0.1.8',
             requires = { {'nvim-lua/plenary.nvim'} }
         }
+        
+        use {
+            'nvim-treesitter/nvim-treesitter',
+            run = function()
+                local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+                ts_update()
+            end,
+        }
         -- Automatically set up your configuration after cloning packer.nvim
         -- Put this at the end after all plugins
         if packer_bootstrap then
