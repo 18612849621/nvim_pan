49e3ab0effdcdb1589ccf6c9a288a7ba9318b2f7
18612849621 <1354368242@qq.com>
2024-07-02 21:13:23 +0800

    add lua_ls

--- lua/plugin_configs/lsp.lua
+++ lua/plugin_configs/lsp.lua
@@ -61,6 +61,9 @@ end
 -- 1. use `:Mason` to install corresponding LSP
 -- 2. add configuration below
 lspconfig.pylsp.setup({
-	on_attach = on_attach,
+    on_attach = on_attach,
 })
 
+lspconfig.lua_ls.setup({
+    on_attach = on_attach,
+})
